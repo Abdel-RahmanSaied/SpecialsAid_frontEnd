744c0e85fabda92519e6223b1b889a6cba8130a1
Yousef Elmesalamy <115108581+yousefelmesalamy@users.noreply.github.com>
2023-05-18 13:44:52 +0300

    create 2 ui's and its views

--- compile.sh
+++ compile.sh
@@ -2,6 +2,8 @@ pyuic5 forms/main_view.ui -o views/main_view.py
 pyuic5 forms/home_view.ui -o views/home_view.py
 pyuic5 forms/collection_view.ui -o views/collection_view.py
 pyuic5 forms/collections_view.ui -o views/collections_view.py
+pyuic5 forms/my_symbols_view.ui -o views/my_symbols_view.py
+pyuic5 forms/welcome_view.ui -o views/welcome_view.py
 
 
 pyrcc5 forms/app_resources.qrc -o app_resources_rc.py
--- views_mangers/welcomeView_Manger.py
+++ views_mangers/welcomeView_Manger.py
@@ -1,24 +1,24 @@
 from PyQt5 import QtWidgets, QtCore
 from views import home_view
 from PyQt5 import QtWidgets
-from views import home_view
+from views import welcome_view
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import *
 import requests
 from PyQt5.QtGui import QPixmap, QPainter
 
 
-class HomeScreen(QtWidgets.QWidget, home_view.Ui_MainWindow):
+class WelcomeScreen(QtWidgets.QWidget, welcome_view.Ui_Form):
     loginAcceptedSignal = QtCore.pyqtSignal()
 
     def __init__(self):
-        super(HomeScreen, self).__init__()
+        super(WelcomeScreen, self).__init__()
         self.setupUi(self)
 
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    w = HomeScreen()
+    w = WelcomeScreen()
     w.show()
     app.exec_()
