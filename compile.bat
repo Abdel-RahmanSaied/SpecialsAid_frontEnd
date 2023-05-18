ad886ca989e740560a01e4e0b2cc07a6d5f77ac1
Yousef Elmesalamy <115108581+yousefelmesalamy@users.noreply.github.com>
2023-05-18 13:50:00 +0300

    create 2 ui's and its views

---
+++ compile.bat
@@ -0,0 +1,9 @@
+pyuic5 forms/main_view.ui -o views/main_view.py
+pyuic5 forms/home_view.ui -o views/home_view.py
+pyuic5 forms/collection_view.ui -o views/collection_view.py
+pyuic5 forms/collections_view.ui -o views/collections_view.py
+pyuic5 forms/my_symbols_view.ui -o views/my_symbols_view.py
+pyuic5 forms/welcome_view.ui -o views/welcome_view.py
+
+
+pyrcc5 forms/app_resources.qrc -o app_resources_rc.py
