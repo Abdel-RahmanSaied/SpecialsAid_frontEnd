491c49835a55eaf7b21f775ca0cabf9582098c1b
Abdel-RahmanSaied <abdo.am169@gmail.com>
2023-05-24 05:02:36 +0300

    my Symbol Screen finished , responsive, dynamic و حسبي الله و نعم الوكيل

--- views_mangers/mySymbolsView_manger.py
+++ views_mangers/mySymbolsView_manger.py
@@ -44,50 +44,104 @@ class MySymbolsScreen(QtWidgets.QWidget, my_symbols_view.Ui_Form):
         self.setupUi(self)
         self.base_url = "https://specialaid.pythonanywhere.com/"
         self.firstTime = True
-        self.symbol_frame = self.frame
-        self.symbol_icon = self.symbol_icon_btn
-        self.symbol_label = self.label_2
         self.threadpool = QtCore.QThreadPool()
-        self.dataCount = 0
-        self.num_of_copies = 0
         self.run()
-        self.copied_frames = []
-        self.create_copies_of_frame()
 
     def run(self):
         worker = ApiWorker(self.base_url + "symbols/symbols/")
         worker.setAutoDelete(True)
         self.loading = WaitingScreen()
-        worker.signals.finished.connect(self.handleSymbols)
+        worker.signals.finished.connect(self.handleFrameContent)
         self.loading.show()
         self.threadpool.start(worker)
 
-    def handleSymbols(self, data):
+    # def handleSymbols(self, data):
+    #     # print(data)
+    #     dataCount = data['count']
+    #     # self.create_copies_of_frame(dataCount)
+    #     print(data['count'])
+    #     # for i in range(dataCount):
+    #     #     self.symbol_icon.clicked.connect(lambda ch, i=i: self.handleCollectionBtn(data['results'][i]['id']))
+    #     #     self.threadpool.globalInstance().findChild(QtCore.QThreadPool, 'globalInstance')
+    #     #     self.threadpool.start(
+    #     #         lambda i = i: self.setButtonIcon(self.symbol_icon[i], data['results'][i]['image']))
+    #     #     self.threadpool.start(lambda i = i: self.setTextLabel(self.symbol_label[i], data['results'][i]['name']))
+    #     self.loading.close()
+
+    def handleFrameContent(self, data):
         print(data)
-        self.dataCount = data['count']
-        print(data['count'])
-        # for i in range(dataCount):
-        #     self.symbol_icon.clicked.connect(lambda ch, i=i: self.handleCollectionBtn(data['results'][i]['id']))
-        #     self.threadpool.globalInstance().findChild(QtCore.QThreadPool, 'globalInstance')
-        #     self.threadpool.start(
-        #         lambda i = i: self.setButtonIcon(self.symbol_icon[i], data['results'][i]['image']))
-        #     self.threadpool.start(lambda i = i: self.setTextLabel(self.symbol_label[i], data['results'][i]['name']))
+        dataCount = data['count']
+        for id in range(dataCount):
+            print(data['results'][id])
+            frame = self.addFrame(id)
+            layoutWidget = self.addLayoutWidget(frame, id)
+            gridLayout = self.addGridLayout(layoutWidget, id)
+            label = self.addLabel(layoutWidget, id)
+            button = self.addSymbolButton(layoutWidget, id)
+            self.addGridLayoutWidget_forBtn(gridLayout, button, id)
+            self.addGridLayoutWidget_forLabel(gridLayout, label, id)
+            self.gridLayout_4.addWidget(frame, id // 5, id % 5)  # Add the frame to the grid layout
+
+            self.setFrameContentData(button, label, data, id)
+            # self.threadpool.globalInstance().findChild(QtCore.QThreadPool, 'globalInstance')
+            # self.threadpool.start(lambda id=id: self.setButtonIcon(button, data['results'][id]['image']))
+            # self.threadpool.start(lambda id=id: self.setTextLabel(label, data['results'][id]['name']))
+            # button.clicked.connect(lambda ch, id=id: self.handleCollectionBtn(data['results'][id]['id']))
+
         self.loading.close()
 
-    def create_copies_of_frame(self):
-        original_stylesheet = self.symbol_frame.styleSheet()
-        for i in range(self.dataCount):
-            copied_frame = self.create_frame_copy()
-            copied_frame.setStyleSheet(original_stylesheet)
-            self.copied_frames.append(copied_frame)
-        print(len(self.copied_frames))
-        return self.copied_frames
+    def addFrame(self, id):
+        frame = QtWidgets.QFrame(self.scrollAreaWidgetContents)
+        frame.setMinimumSize(QtCore.QSize(135, 170))
+        frame.setMaximumSize(QtCore.QSize(135, 180))
+        frame.setStyleSheet("")
+        frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        frame.setObjectName(f"frame_{id}")
+        return frame
 
-    def create_frame_copy(self):
-        copied_frame = QtWidgets.QFrame(self.scrollArea)
-        copied_frame.setGeometry(self.symbol_frame.geometry())
-        copied_frame.setStyleSheet(self.symbol_frame.styleSheet())
-        return copied_frame
+    def addLayoutWidget(self, frame, id):
+        layoutWidget = QtWidgets.QWidget(frame)
+        layoutWidget.setGeometry(QtCore.QRect(0, 0, 132, 175))
+        layoutWidget.setObjectName(f"layout_{id}")
+        return layoutWidget
+
+    def addGridLayout(self, layoutWidget, id):
+        gridLayout = QtWidgets.QGridLayout(layoutWidget)
+        gridLayout.setContentsMargins(0, 0, 0, 0)
+        gridLayout.setObjectName(f"gridLayout_{id}")
+        return gridLayout
+
+    def addSymbolButton(self,layoutWidget, id):
+        symbol_icon_btn = QtWidgets.QPushButton(layoutWidget)
+        symbol_icon_btn.setMinimumSize(QtCore.QSize(130, 150))
+        symbol_icon_btn.setMaximumSize(QtCore.QSize(132, 150))
+        symbol_icon_btn.setStyleSheet("background-color: rgb(255, 255, 255,0);\n"
+                                           "\n"
+                                           "border-color: rgb(255, 255, 255);")
+        symbol_icon_btn.setText("")
+        symbol_icon_btn.setObjectName(f"symbol_icon_btn_{id}")
+        return symbol_icon_btn
+
+    def addLabel(self, layoutWidget, id):
+        label = QtWidgets.QLabel(layoutWidget)
+        label.setAlignment(QtCore.Qt.AlignCenter)
+        label.setObjectName(f"label_{id}")
+        label.setText("Label")
+        return label
+
+    def addGridLayoutWidget_forBtn(self, gridLayout, symbol_icon_btn, id):
+        gridLayout.addWidget(symbol_icon_btn, 0, 0, 1, 1)
+
+    def addGridLayoutWidget_forLabel(self, gridLayout, symbol_label, id):
+        gridLayout.addWidget(symbol_label, 1, 0, 1, 1)
+
+
+    def setFrameContentData(self, btn, label, data, id):
+        btn.clicked.connect(lambda ch, id=id: self.handleCollectionBtn(data['results'][id]['id']))
+        self.threadpool.globalInstance().findChild(QtCore.QThreadPool, 'globalInstance')
+        self.threadpool.start(lambda id=id: self.setButtonIcon(btn, data['results'][id]['image']))
+        self.threadpool.start(lambda id=id: self.setTextLabel(label, data['results'][id]['name']))
 
     def setButtonIcon(self, btn, url):
         photo = self.get_image(url)
@@ -96,11 +150,12 @@ class MySymbolsScreen(QtWidgets.QWidget, my_symbols_view.Ui_Form):
         btn.setIconSize(QtCore.QSize(180, 180))
 
     def setTextLabel(self, label, text):
-        label = self.findChild(QtWidgets.QLabel, label)
+        # label = self.findChild(QtWidgets.QLabel, label)
         label.setText(text)
 
     def handleCollectionBtn(self, id):
         self.symbolIDSignal.emit(id)
+        print(id)
 
     def get_image(self, url):
         response = requests.get(url)
